00ac541db2a12fe7ce94bc17c53a7758bee9d574
Facundo Rossetti <facundorossetti@outlook.com>
2022-09-08 18:13:13 -0300

    make the app works

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,22 @@
 const { Router } = require('express')
 const {
   getUsers,
-  getUserById,
-  createUser,
-  deleteUserById,
+  // getUserById,
+  // createUser,
+  // deleteUserById,
 } = require('../controllers/index.controller')
 
 const router = Router()
 
 // GET
 router.get('/users', getUsers);
-router.get('/users/:id', getUserById);
+// router.get('/users/:id', getUserById);
 
-// POST
-router.post('/users', createUser);
+// // POST
+// router.post('/users', createUser);
 
-// DELETE
-router.delete('/users/:id', deleteUserById);
+// // DELETE
+// router.delete('/users/:id', deleteUserById);
 
 
-module.exports = router
+module.exports = router;
