77b0d81b6a3301d35333b22ec1df1a9bb4621980
Facundo Rossetti <facundorossetti@outlook.com>
2022-09-11 23:34:25 -0300

    backend test mercadopago

--- database/database.sql
+++ database/database.sql
@@ -1,2 +1,3 @@
 CREATE TABLE products(id INT PRIMARY KEY, type VARCHAR(40), description VARCHAR(100), color VARCHAR(10), stock JSON, price DECIMAL(10,2), imageURL varchar(255));
 
+CREATE TABLE mporders(id INT, DateCreated DATETIME NOT NULL DEFAULT(GETDATE()));
